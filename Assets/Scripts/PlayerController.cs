91f9c448ebdc564efa5ccb1b2e41f9cf9fefc382
Óscar <oscargalbae@gmail.com>
2024-09-20 10:54:47 +0200

    Programación Personaje

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D characterRigdbody;
+    private float horizontalInput;
+    [SerializeField]private float characterSpeed = 4.5f;
+    [SerializeField]private float jumpforce = 5f;
+
+    void Awake()
+    {
+        characterRigdbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // characterRigdbody.AddForce(Vector2.up * jumpforce);
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            characterRigdbody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        characterRigdbody.velocity = new Vector2(horizontalInput * characterSpeed, characterRigdbody.velocity.y);
+        
+        
+
+    }
+
+}
