833e089386eb11b7c3eb7408ad6138c1b8c9df02
GALYBAN <oscargalbae@gmail.com>
2024-09-30 13:47:31 +0200

    Ejercicio 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]private float characterSpeed = 4.5f;
     [SerializeField]private float jumpforce = 5f;
+
+    private bool isAttacking;
 
     void Awake()
     {
@@ -30,28 +33,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
 
-        if(horizontalInput < 0)
+        Movement();
+
+        if(Input.GetButtonDown("Jump") && GroundSensor.isGrounded && !isAttacking)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            characterAnimator.SetBool("IsRunning", true);
+            Jump();
         }
 
-        else if(horizontalInput > 0)
+        if(Input.GetButtonDown("Fire1") && GroundSensor.isGrounded)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            characterAnimator.SetBool("IsRunning", true);
-        }
-
-        else
-        {
-            characterAnimator.SetBool("IsRunning", false);
-        }
-
-        if(Input.GetButtonDown("Jump") && GroundSensor.isGrounded)
-        {
-            characterRigdbody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+            Attack();
         }
 
     }
@@ -60,24 +52,95 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         characterRigdbody.velocity = new Vector2(horizontalInput * characterSpeed, characterRigdbody.velocity.y);
-        
-        
-
     }
+    void Movement()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(horizontalInput == 0)
+        {
+            characterAnimator.SetBool("IsRunning", false);
+        }
+
+        if(isAttacking)
+        {
+            return;
+        }
+        else if(horizontalInput < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            characterAnimator.SetBool("IsRunning", true);
+        }
+        else if(horizontalInput > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            characterAnimator.SetBool("IsRunning", true);
+        }
+
+        /*if(horizontalInput < 0)
+        {
+            if(!isAttacking)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            characterAnimator.SetBool("IsRunning", true);
+        }
+
+        else if(horizontalInput > 0)
+        {
+            if(!isAttacking)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            characterAnimator.SetBool("IsRunning", true);
+        }
+
+        else
+        {
+            characterAnimator.SetBool("IsRunning", false);
+        }*/
+    }
+
+    void Jump()
+    {
+        characterRigdbody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+    }
+
+    void Attack()
+    {
+        StartCoroutine(AttackingAnimation());
+        characterAnimator.SetTrigger("IsAttacking");
+    }
+
+    IEnumerator AttackingAnimation()
+    {
+        isAttacking = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        isAttacking = false;
+    }
+
     void TakeDamage()
     {
         healthPoints--;
-        characterAnimator.SetTrigger("IsHurt");
-        if(healthPoints == 0)
+
+        if(healthPoints <= 0)
         {
             Die();
+            return;
         }
+
+        characterAnimator.SetTrigger("IsHurt");
+
+        
 
     }
 
      void Die()
     {
-        characterAnimator.SetBool("IsDead", true);
+        characterAnimator.SetTrigger("IsDead");
         Destroy(gameObject, 0.45f);
     }
 
